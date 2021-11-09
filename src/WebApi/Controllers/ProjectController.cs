ba99a762310d0f34187369593732a58b4d4d2228
Adi Irawan <adiirawan32@gmail.com>
2021-11-09 16:08:36 +0700

    fixed dto api

--- src/WebApi/Controllers/ProjectController.cs
+++ src/WebApi/Controllers/ProjectController.cs
@@ -1,5 +1,5 @@
 ﻿using ApplicationCore.Interfaces.Projects;
-using ApplicationCore.DTOs.Projects.Dto;
+using ApplicationCore.DTOs.Projects;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
